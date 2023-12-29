c150574e5742bec9fb92d21fe7aec148a78f62c2
Gerardo Miranda <gerardodev25@gmail.com>
2023-12-29 19:57:33 -0400

    chore(TanStack): :construction: useLabels + useQuery
    useLabels + useQuery
    
    useLabels + useQuery

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,27 +1,26 @@
-import { useQuery } from '@tanstack/react-query';
-import { githubApi } from '../../api/githubApi';
-import { Label } from '../interfaces/label';
-
-const getLabels = async (): Promise<Label[]> => {
-  const { data } = await githubApi.get<Label[]>('/labels');
-  return data;
-};
+import { useLabels } from '../hooks/useLabels';
 
 export const LabelPicker = () => {
-  const labelsQuery = useQuery({
-    queryKey: ['labels'],
-    queryFn: getLabels,
-    refetchOnWindowFocus: false,
-  });
+  const labelsQuery = useLabels();
+
+  if (labelsQuery.isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
-    <div>
-      <span
-        className='badge rounded-pill m-1 label-picker'
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-      >
-        Primary
-      </span>
-    </div>
+    <>
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className='badge rounded-pill m-1 label-picker'
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+        >
+          {label.name}
+        </span>
+      ))}
+    </>
   );
 };
---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../../api/githubApi';
+import { Label } from '../interfaces/label';
+import { sleep } from '../../helpers/sleep';
+
+const getLabels = async (): Promise<Label[]> => {
+
+  await sleep(2)
+  
+  const { data } = await githubApi.get<Label[]>('/labels');
+  return data;
+};
+
+export const useLabels = () => {
+  const labelsQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels,
+    refetchOnWindowFocus: false,
+  });
+
+  return labelsQuery;
+};
