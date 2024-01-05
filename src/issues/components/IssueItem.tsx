a25a18538d55552b03d0f130173e88d98f5a6cd1
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-04 21:33:11 -0400

    chore(TanStack): :construction: Navegar y cargar issue por número
    Navegar y cargar issue por número
    
    Navegar y cargar issue por número

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,22 +1,32 @@
 import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
-import { Issue } from '../interfaces';
+import { Issue, State } from '../interfaces';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   issue: Issue;
 }
 
 export const IssueItem: FC<Props> = ({ issue }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className='card mb-2 issue'>
+    <div
+      className='card mb-2 issue'
+      onClick={() => navigate(`/issues/issue/${issue.number}`)}
+    >
       <div className='card-body d-flex align-items-center'>
         <FiInfo size={30} color='red' />
-        {/* <FiCheckCircle size={30} color="green" /> */}
+        {issue.state === State.Open ? (
+          <FiCheckCircle size={30} color='red' />
+        ) : (
+          <FiCheckCircle size={30} color='green' />
+        )}
 
         <div className='d-flex flex-column flex-fill px-2'>
           <span>{issue.title}</span>
           <span className='issue-subinfo'>
-            #25581 opened 2 days ago by{' '}
+            #{issue.number} opened 2 days ago by{' '}
             <span className='fw-bold'>{issue.user.login}</span>
           </span>
         </div>
