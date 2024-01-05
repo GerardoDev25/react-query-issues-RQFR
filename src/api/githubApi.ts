e35c22d6ea5c7f5102d96bb769acd9cf62ea9e4c
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-05 18:16:20 -0400

    chore(TanStack): :construction: QueryClient - prefetchQuery
    QueryClient - prefetchQuery
    
    QueryClient - prefetchQuery

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization: `Bearer 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`,
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_KEY}`,
   },
 });
--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,7 +1,10 @@
-import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
-import { Issue, State } from '../interfaces';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
+
+import { Issue, State } from '../interfaces';
+import { getIssueInfo } from '../hooks';
 
 interface Props {
   issue: Issue;
@@ -9,11 +12,20 @@ interface Props {
 
 export const IssueItem: FC<Props> = ({ issue }) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const onMouseEnter = () => {
+    queryClient.prefetchQuery({
+      queryKey: ['issue', issue.number],
+      queryFn: () => getIssueInfo(issue.number),
+    });
+  };
 
   return (
     <div
       className='card mb-2 issue'
       onClick={() => navigate(`/issues/issue/${issue.number}`)}
+      onMouseEnter={onMouseEnter}
     >
       <div className='card-body d-flex align-items-center'>
         <FiInfo size={30} color='red' />
