44e6eb2f343d2e7ed97d4534ddc2ad5d64b24f99
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-05 19:24:05 -0400

    chore(TanStack): :construction: Actualizar la fecha de creación
    Actualizar la fecha de creación
    
    Actualizar la fecha de creación

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -5,6 +5,7 @@ import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
 
 import { Issue, State } from '../interfaces';
 import { getIssueComments, getIssueInfo } from '../hooks';
+import { timeSince } from '../../helpers';
 
 interface Props {
   issue: Issue;
@@ -51,7 +52,7 @@ export const IssueItem: FC<Props> = ({ issue }) => {
         <div className='d-flex flex-column flex-fill px-2'>
           <span>{issue.title}</span>
           <span className='issue-subinfo'>
-            #{issue.number} opened 2 days ago by{' '}
+            #{issue.number} opened {timeSince(issue.created_at)} ago by:{' '}
             <span className='fw-bold'>{issue.user.login}</span>
           </span>
           <div>
